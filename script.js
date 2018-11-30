1df9bddf9b4065334eadb05a0fc1e21f538c11e9
mjyu1024 <yliu333@id.iit.edu>
2018-11-30 12:57:23 -0600

    Route2

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ let canvas = document.querySelector("#play-ground");
 let ctx = canvas.getContext("2d");
 const statusElement = document.querySelector("#status");
 
-let playerX = 10;
+// set up player & speed
+let playerX = 20;
 let playerY = 140;
 let speed = 10;
 let jump = 20;
 
+// set up draw function
 const drawPlayer = (x, y) => {
     ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
     ctx.ellipse(x, y, 8, 10, 0, Math.PI, 2 * Math.PI);
     ctx.lineTo(x + 4, y);
     ctx.lineTo(x + 4, y + 10);
@@ -27,34 +31,75 @@ const drawPlayer = (x, y) => {
 
 const drawRoute = (x, y) => {
     ctx.beginPath();
-    ctx.rect(x - 5, y - 5, 10, 10);
     ctx.fillStyle = "black";
-    ctx.fill();
-    ctx.stroke();
+    ctx.fillRect(x - 5, y - 5, 10, 10);
+
 }
 
-const shapes = [];
 const drawSVG = (svg) => {
     return new Path2D(svg);
 };
-let path = drawSVG("M20 140h60V80H20V20h60M100 20h30v120m30-120h-30M340 20h30v120m30-120h-30M180 140V20h60v120m-60-60h60M580 140V20h60v120m-60-60h60M260 140V20h60v60h-50v10h10v10h10v10h10v10h10v10h10v10M560 20h-60v120h60V80h-30M660 140V20h10v30h10v30h20V50h10V20h10v120M800 140h-60V20h60m-30 60h-30");
-let shape = {
-    name: "startGame",
-    path: path,
+let path0 = drawSVG("M20 140h60V80H20V20h60M100 20h30v120m30-120h-30M340 20h30v120m30-120h-30M180 140V20h60v120m-60-60h60M260 140V20h60v60h-50v10h10v10h10v10h10v10h10v10h10v10");
+let hintPath0 = drawSVG("M580 140V20h60v120m-60-60h60M560 20h-60v120h60V80h-30M660 140V20h10v30h10v30h20V50h10V20h10v120M800 140h-60V20h60m-30 60h-30");
+
+const drawHint = (hint) => {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 10;
+    ctx.stroke(hint);
 };
-shapes.push(shape);
+
+const drawPath = (shape) => {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 10;
+    ctx.stroke(shape);
+};
+
+const shapes = [];
+let shape0 = {
+    name: "start",
+    path: path0,
+};
+shapes.push(shape0);
+
+const hints = [];
+let hint0 = {
+    name: "game",
+    path: hintPath0,
+};
+hints.push(hint0);
 
 const routes = [];
+let route0 = {
+    x: playerX,
+    y: playerY,
+}
+routes.push(route0);
+
+const addToRoutes = (route) => {
+    let found = routes.some(function (r) {
+        return r.x === route.x && r.y === route.y;
+    });
+
+    if (!found) {
+        routes.push(route);
+    }
+}
 
 const step = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < shapes.length; i++) {
-        ctx.stroke(shapes[i].path);
+    for (let m = 0; m < hints.length; m++) {
+        drawHint(hints[m].path);
     }
-    for (let n = 0; n < routes.length; n++) {
+
+    for (let i = 0; i < shapes.length; i++) {
+        drawPath(shapes[i].path);
+    }
+
+    for (let n = 1; n < routes.length; n++) {
         let route = routes[n];
         drawRoute(route.x, route.y);
     }
+    drawRoute(routes[0].x, routes[0].y);
     drawPlayer(playerX, playerY);
     window.requestAnimationFrame(step);
 };
@@ -86,7 +131,13 @@ const onTrack = (event) => {
                 x: playerX,
                 y: playerY,
             };
-            routes.push(route);
+            // for (let n = 0; n < routes.length; n++) {
+            //     console.log(route, routes[n]);
+            //     if (route.x !== routes[n].x || route.y !== routes[n].y) {
+            //         routes.push(route);
+            //     }
+            // }
+            addToRoutes(route);
         } else {
             statusElement.innerText = "Outside Shape";
         }
